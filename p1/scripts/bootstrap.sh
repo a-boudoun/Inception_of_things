d2aa0265b82b0ce0f1405a552283ae4479020572
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-10-20 00:01:13 +0100

    set up UFW to install k3s

--- p1/scripts/bootstrap.sh
+++ p1/scripts/bootstrap.sh
@@ -3,4 +3,9 @@
 
 echo "Starting the shell provisioner..."
 
+# sudo ufw disable
+
+ufw allow 6443/tcp #apiserver
+ufw allow from 10.42.0.0/16 to any #pods
+ufw allow from 10.43.0.0/16 to any #services
 
