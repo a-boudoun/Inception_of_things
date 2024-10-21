32acf0a37878e5668b48d8bda6e20392720f03d3
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-10-21 14:22:49 +0100

    clean the bootstrap script to set up k3s server (shell provisioning is done)

--- p1/scripts/bootstrap_vm1.sh
+++ p1/scripts/bootstrap_vm1.sh
@@ -12,13 +12,6 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # set up k3S
 echo "Set up K3S..."
 
-# set up ufw for k3s
-# sudo ufw disable
-
-# ufw allow 6443/tcp #apiserver
-# ufw allow from 10.42.0.0/16 to any #pods
-# ufw allow from 10.43.0.0/16 to any #services
-
 # install k3s server
 
 curl -sfL https://get.k3s.io | sh -
@@ -30,9 +23,3 @@ sudo chmod 777 /var/lib/rancher/k3s/server/
 sudo cat /var/lib/rancher/k3s/server/node-token | tr -d '\n' > /vagrant/confs/node.txt
 
 sudo hostname -I | awk '{print $2}' | tr -d '\n' > /vagrant/confs/hostname.txt
-
-# run server
-sudo k3s server
-
-
-# no conection with k3s, potemtial problem with the firewall
