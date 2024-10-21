155e573b88dda9131d12755d7d5ef30e52ba8fea
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-10-21 11:46:47 +0100

    installing kubectl for both machines

--- p1/scripts/bootstrap_vm1.sh
+++ p1/scripts/bootstrap_vm1.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-
+# Start shell provisioning
 echo "Starting the shell provisioner for the first machine..."
+
+# install Kubectl
+echo "Installing Kubectl..."
+curl -LO "https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+# set up k3S
+echo "Set up K3S..."
 
 # set up ufw for k3s
 # sudo ufw disable
@@ -23,5 +32,7 @@ sudo cat /var/lib/rancher/k3s/server/node-token | tr -d '\n' > /vagrant/confs/no
 sudo hostname -I | awk '{print $2}' | tr -d '\n' > /vagrant/confs/hostname.txt
 
 # run server
+sudo k3s server
 
-# sudo k3s server
+
+# no conection with k3s, potemtial problem with the firewall
--- p1/scripts/bootstrap_vm2.sh
+++ p1/scripts/bootstrap_vm2.sh
@@ -2,6 +2,12 @@
 
 echo "Starting the shell provisionner for the second machine..."
 
+# install Kubectl
+echo "Installing Kubectl..."
+curl -LO "https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Set up K3S..."
 SERVER_IP=$(</vagrant/confs/hostname.txt)
 TOKEN=$(</vagrant/confs/node.txt)
 
@@ -9,6 +15,4 @@ echo "Hostname: $SERVER_IP"
 echo "Token: $TOKEN"
 
 
-curl -sfL https://get.k3s.io | K3S_URL=https://SERVER_IP:6443 K3S_TOKEN=TOKEN sh -
-
-# sudo k3s agent --server https://myserver:6443 --token ${TOKEN}
+curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$TOKEN sh -
