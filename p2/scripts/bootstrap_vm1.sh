520d2b4e28a04b0b7091885049817aef174baabd
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-11-09 23:00:56 +0100

    update boostrap script for p2

--- p2/scripts/bootstrap_vm1.sh
+++ p2/scripts/bootstrap_vm1.sh
@@ -3,19 +3,17 @@
 # Start shell provisioning
 echo "Starting the shell provisioner for the first machine..."
 
-# install Kubectl
-echo "Installing Kubectl..."
-curl -LO "https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
+# install necessary tools
+echo "Installing necesary tools..."
+sudo apt install net-tools
 
 # set up k3S
 echo "Set up K3S..."
 
 # install k3s server
-
 curl -sfL https://get.k3s.io | sh -
 
+# use 644 for read-only access
 sudo chmod 777 /etc/rancher/k3s/k3s.yaml
 
 sudo chmod 777 /var/lib/rancher/k3s/server/
@@ -23,3 +21,4 @@ sudo chmod 777 /var/lib/rancher/k3s/server/
 sudo cat /var/lib/rancher/k3s/server/node-token | tr -d '\n' > /vagrant/confs/node.txt
 
 sudo hostname -I | awk '{print $2}' | tr -d '\n' > /vagrant/confs/hostname.txt
+
