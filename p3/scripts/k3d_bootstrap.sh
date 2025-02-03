78cf75a9c7c5847a159fcb2312e6d41aa9d054df
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2025-02-03 19:56:53 +0100

    update path for the k3d cluster in the script

--- p3/scripts/k3d_bootstrap.sh
+++ p3/scripts/k3d_bootstrap.sh
@@ -40,7 +40,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 echo "Install up K3d..."
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-k3d cluster create --config ../confs/iot-k3d-cluster.yaml
+k3d cluster create --config ../confs/k3d_cluster_config/iot-k3d-cluster.yaml
 
 # creating the namespaces
 kubectl create namespace dev && kubectl create namespace argocd
