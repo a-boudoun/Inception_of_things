22202c72eacc44a5dba6138bb1aec0b3e265b22f
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2025-03-14 03:25:12 +0000

    docker should be setup before

--- p3/scripts/k3d_bootstrap.sh
+++ p3/scripts/k3d_bootstrap.sh
@@ -7,30 +7,6 @@ echo "Setup for K3d requirments..."
 echo "Installing necesary tools..."
 sudo apt install net-tools
 
-# install docker for a ubuntu machine
-echo "Installing Docker"
-
-# clean any old packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-# install docker plugins
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# need to setup docker to run without sudo
 
 # install Kubectl to interact with K3S cluster
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
