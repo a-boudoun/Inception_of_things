5e279547c1de2ab8c4b3e582bf93ada18894c9ae
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2025-03-16 23:33:41 +0000

    chore: modes redefined

--- p2/scripts/bootstrap_vm1.sh
+++ p2/scripts/bootstrap_vm1.sh
@@ -13,11 +13,6 @@ echo "Set up K3S..."
 # install k3s server
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 644" sh -
 
-# use 644 for read-only access
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-
-sudo chmod 644 /var/lib/rancher/k3s/server/
-
 # create the k3s resources for the apps
 echo "Creating the deployments..."
 sudo kubectl create -f /vagrant/confs/
